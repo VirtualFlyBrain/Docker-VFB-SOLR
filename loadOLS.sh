6baa9e59a13d8d78ca33586b9e650ece6efb6487
Rob Court <robbie.court@gmail.com>
2018-01-11 19:18:53 +0000

    debug

--- loadOLS.sh
+++ loadOLS.sh
@@ -4,6 +4,12 @@ init-solr-home
 
 solr start -p 8984
 
+curl -sSf http://localhost:8983/solr
+
+curl -sSf http://localhost:8983/solr/ontology
+
+curl -sSf http://localhost:8983/solr/ontology/select
+
 java -Xmx2g -jar -Dspring.profiles.active=fbbt ${WORKSPACE}/OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar
 
 solr stop
