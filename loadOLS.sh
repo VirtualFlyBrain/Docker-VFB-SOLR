fe18289a70dc107f75b06fa337b8c358b01480dd
Rob Court <robbie.court@gmail.com>
2018-01-11 12:43:50 +0000

    loading ontology

--- loadOLS.sh
+++ loadOLS.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+start-local-solr
+
+java -Xmx2g -jar -Dspring.profiles.active=fbbt ${WORKSPACE}/OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar
+
+stop-local-solr
 
 solr-foreground
