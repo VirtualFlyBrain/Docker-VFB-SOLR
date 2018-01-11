6bb88d135397f0f2665536601adcb90760481fda
Rob Court <robbie.court@gmail.com>
2018-01-11 17:22:15 +0000

    creating cores

--- loadOLS.sh
+++ loadOLS.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+solr-create -c ontology -d /ontology
+solr-create -c autosuggest -d /autosuggest
+
 solr start -p 8984 -Dsolr.solr.home=${SOLR_HOME}
 
 java -Xmx2g -jar -Dspring.profiles.active=fbbt ${WORKSPACE}/OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar
