614270ec50b1ab3cd2cb8a85a2eb7149e5740967
Rob Court <robbie.court@gmail.com>
2018-01-11 18:00:12 +0000

    adding dependancies

--- loadOLS.sh
+++ loadOLS.sh
@@ -3,6 +3,8 @@
 precreate-core ontology ${WORKSPACE}/OLS/ols-solr/src/main/solr-5-config/ontology
 precreate-core autosuggest ${WORKSPACE}/OLS/ols-solr/src/main/solr-5-config/autosuggest
 
+cp -f /opt/VFB/OLS/ols-solr/src/main/solr-5-config/solr.xml /opt/solr/server/solr/
+
 solr start -p 8984
 
 java -Xmx2g -jar -Dspring.profiles.active=fbbt ${WORKSPACE}/OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar
