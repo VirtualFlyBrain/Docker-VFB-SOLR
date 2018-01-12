617732edfe48512809c14cc1ee913ce34af354d4
Rob Court <robbie.court@gmail.com>
2018-01-12 10:05:32 +0000

    swapping to vfb

--- loadOLS.sh
+++ loadOLS.sh
@@ -10,7 +10,7 @@ curl -sSf http://localhost:8983/solr/ontology
 
 curl -sSf http://localhost:8983/solr/ontology/select
 
-java -Xmx2g -jar -Dspring.profiles.active=fbbt ${WORKSPACE}/OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar
+java -Xmx2g -jar -Dspring.profiles.active=vfb ${WORKSPACE}/OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar
 
 solr stop
 
